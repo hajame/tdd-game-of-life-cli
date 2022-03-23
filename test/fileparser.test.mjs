287d128ff69f3bf551452fd6dbe4c9d1f09f32d5
hajame <harri.mehtala@helsinki.fi>
2022-03-23 14:36:21 +0200

    more fileparser tests

--- test/fileparser.test.mjs
+++ test/fileparser.test.mjs
@@ -23,4 +23,21 @@ describe("Fileparser", () => {
       expect(fileparser.pattern).to.equal("3b$3b$3b!");
     });
   });
+  describe("Parse file with comments and other stuff", () => {
+    beforeEach(() => {
+      fileparser = new FileParser(
+        `#N Just an empty board\n#C This was the first test.\nx = ${3}, y = ${3}\n3b$3b$3b!`
+      );
+      fileparser.parse();
+    });
+    it("y is correct", () => {
+      expect(fileparser.y).to.equal(3);
+    });
+    it("x is correct", () => {
+      expect(fileparser.x).to.equal(3);
+    });
+    it("pattern is correct", () => {
+      expect(fileparser.pattern).to.equal("3b$3b$3b!");
+    });
+  });
 });
