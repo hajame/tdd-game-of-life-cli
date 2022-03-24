256caa39436589fa4f7f317082f671c1cc1f9db6
hajame <harri.mehtala@helsinki.fi>
2022-03-24 10:19:09 +0200

    (continue) refactor longform conversion - failing test

---
+++ test/rleConverter.test.mjs
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { RleConverter } from "../src/rleConverter.mjs";
+
+let rleConverter;
+
+function getRleConverter(pattern) {
+  return new RleConverter(pattern);
+}
+
+describe("RleConverter", () => {
+  describe("Convert to long form", () => {
+    beforeEach(() => {
+      rleConverter = getRleConverter("3b$3b$3b!");
+      rleConverter.parse();
+    });
+    it("output is correct", () => {
+      expect(rleConverter.toLongForm()).to.equal("bbb$bbb$bbb!");
+    });
+  });
+});
